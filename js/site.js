08a7deed93736c196efa93764d432770a7573582
dev-vader091 <86165493+dev-vader091@users.noreply.github.com>
2023-01-18 17:16:13 -0600

    changed layout and added JS functionality

--- js/site.js
+++ js/site.js
@@ -1,11 +1,83 @@
-function displayMessage() {
-  let msg = document.getElementById('message').value;
+// entry point: controller function
+function getValues() {
+  // get user input
+  let inputString = document.getElementById('userString').value;
 
-  Swal.fire(
-    {
-      backdrop: false,
-      title: 'App Name',
-      text: msg
-    }
-  );
+  // if special chars/spaces -- validate/remove special characters -- if() statement
+  let validatedString = removeChar(inputString);
+
+  let checkString = checkForPalindrome(validatedString);
+
+  displayResults(checkString)
+  
+  // if no special chars/spaces -- run checkForPalindrome()
+  // checkForPalindrome(inputString);
+
+ }
+
+
+
+// logic function
+function checkForPalindrome(userInput) {
+  // reverse the string
+  let reversedString = "";
+
+  
+  let notPalindrome;
+  let palindrome ;
+
+  for(i = userInput.length - 1; i >= 0; i--) {
+    let currentLetter = userInput[i];
+
+    reversedString += currentLetter;
+
+  }
+
+  // compare to user input
+  if(reversedString == userInput) {
+    palindrome = reversedString;
+    
+  }else {
+    notPalindrome = reversedString;
+  }
+  // return 
+  return reversedString;
+  
 }
+
+// view function
+function displayResults(resultWord) {
+  // display the result 
+  let alertHeading = document.getElementById('alertHeading');
+  let alertBox = document.getElementById('alert');
+  let resultMessage = document.getElementById('results');
+  
+  let inputString = document.getElementById('userString').value;
+  inputString = removeChar(inputString);
+
+if(resultWord == inputString) {
+  alertHeading.textContent = `Well done, you entered a Palindrome!`;
+  resultMessage.textContent = `Your phrase reversed is: ${resultWord}`;
+  alertBox.classList.remove('invisible');
+  alertBox.classList.remove('alert-danger');
+  alertBox.classList.add('alert-success');
+}
+else {
+  alertHeading.textContent = `Sorry, you didn't enter a Palindrome.`;
+  resultMessage.textContent = `Your phrase reversed is: ${resultWord}`;
+  alertBox.classList.remove('invisible');
+  alertBox.classList.remove('alert-seuccess');
+  alertBox.classList.add('alert-danger');
+}
+
+}
+
+// validation function
+function removeChar(word) {
+   // word with regex goes here
+  let changedString = word.replace(/[^a-zA-Z0-9]/g, '');
+
+  return changedString;
+}
+
+
